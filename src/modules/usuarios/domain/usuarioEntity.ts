54d57013cd6ccc49b7768abb3e3804c550b22b0e
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-03-23 15:31:55 -0300

    O que é validação?

--- src/modules/usuarios/domain/usuarioEntity.ts
+++ src/modules/usuarios/domain/usuarioEntity.ts
@@ -1,8 +1,21 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class Usuario {
     id: number;
+
+    @IsNotEmpty()
+    @IsString()
     nomeDeUsuario: string;
+
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
     senha: string;
+
+    @IsNotEmpty({
+        message: "O nome completo é obrigatório"
+    })
     nomeCompleto: string;
     dataDeEntrada: Date;
 }
